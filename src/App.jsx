1cd466c458dee6176e5146c1d37616b82d772ddb
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2023-10-30 07:55:20 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,51 @@
-// import React from 'react';
 
-// import { useEffect } from 'react';
 import './App.css';
 
 import styled from 'styled-components'
+import  { useState } from 'react';
 
 const App = () => {
 
 
-  const textStyle = {
-    width: '100%', height: '10vh',
-     backgroundColor: 'blue', 
-     padding: '1rem', color: 'white',
-    margin: '1rem'
-  }
+ const [firstName, setFirstName] = useState('Isaac')
+ const [ background, setBackground] = useState('colorB')
+
+  // setFirstName('John')
+ const changeBackground = () => {
+  // toggle 
+    setBackground('colorA')
+ }
 
   const Wrapper = styled.div`
   text-align: center;
   margin: 0 auto;
   margin: 1rem;
+  width: 100%;
   `
 
-  const Title = styled.h1`
-  color: red;
-  font-size: 3rem;
-  `
 
 
   return (
     <>
-     <div style={textStyle}>
-        <h1>React App</h1>
-     </div>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
 
-     <div className="container">
-      <h1> hello</h1>
-     </div>
 
-     <Wrapper className="container">
-      <h1> style component approach</h1>
+     <Wrapper className="container text-center mx-auto mt-4">
+      <div className="flex  justify-between items-center w-8/12 p-3 ">
+      <h1 className="text-2xl"> {firstName} </h1>
+        <button onClick={() => setFirstName('Yeboah') }
+        className="bg-red-900 text-white py-3 px-6 rounded-lg hover:bg-white hover:text-red-800"> change</button>
+      </div>
      </Wrapper>
 
-      <Title>
-        app title goes here
-        </Title>
+     <div className={`container  ${background}  outline-none border-0`}>
+       <div className="flex">
+       <button onClick={() => changeBackground() }
+        className="bg-pink-900 text-white py-3 px-6 rounded-lg hover:bg-white
+         hover:text-red-800"> change background</button>
+      
+         </div>
+     </div>
 
         
     </>
